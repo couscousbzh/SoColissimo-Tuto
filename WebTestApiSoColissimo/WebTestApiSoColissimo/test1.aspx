ee1833c18534ee6bbf61f60a55151fd6ed418b16
couscousbzh <yannvasseur@yahoo.fr>
2015-02-23 16:29:31 +0100

    d

---
+++ WebTestApiSoColissimo/WebTestApiSoColissimo/test1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test1.aspx.cs" Inherits="WebTestApiSoColissimo.test1" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>So Colissimo</title>
+    <style>
+
+        body {
+
+            background-color: #000;
+            font-family: Arial, Helvetica, sans-serif;
+            font-size: 20px;            
+            padding: 25px;            
+        }
+
+    </style>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <asp:Label ID="lblDebug" runat="server" ForeColor="#00ff00"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebTestApiSoColissimo/WebTestApiSoColissimo/test1.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+
+namespace WebTestApiSoColissimo
+{
+    public partial class test1 : System.Web.UI.Page
+    {
+
+        private int account = 0;
+        private int password = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                lblDebug.Text = "";
+
+
+                lblDebug.Text += "Test SoColissimo...<br>";
+                lblDebug.Text += "..............................................................................................................<br><br>";
+
+                lblDebug.Text += "Initiating ...<br>";
+
+                SWSoColissimo.WSColiPosteLetterServiceClient client = new SWSoColissimo.WSColiPosteLetterServiceClient();
+
+
+                SWSoColissimo.AddressVO addressVO = new SWSoColissimo.AddressVO();
+                addressVO.companyName = "Reactor";
+                addressVO.line1 = "2 la Grée";
+                addressVO.postalCode = "35580";
+                addressVO.city = "Guignen";
+
+
+                SWSoColissimo.LetterVO letter = new SWSoColissimo.LetterVO();
+
+                letter.contractNumber = account;
+                letter.contractNumber = password;
+
+                letter.dest = new SWSoColissimo.DestEnvVO();
+                letter.dest.addressVO = addressVO;
+
+                //Faire un test de disponibilité du service 
+
+                lblDebug.Text += "Try to request for a letter<br>";
+
+                SWSoColissimo.ReturnLetterVO returnLetterVO = client.getLetter(letter);
+
+                lblDebug.Text += "Done<br>";
+
+                lblDebug.Text += returnLetterVO.PdfUrl;
+            }
+            catch (Exception ex)
+            {
+                lblDebug.Text += "ERROR<br>";
+                while (ex != null)
+                {
+                    lblDebug.Text += ex.Message + "<br>";
+                    ex = ex.InnerException;                
+                }
+            
+            }
+
+        }
+    }
+}
